35cef7243f85d2655313a452e9f35a8d13f5c09c
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2020-09-05 11:13:20 -0400

    slashify mocks

--- tests/__mocks__/os.js
+++ tests/__mocks__/os.js
@@ -2,10 +2,10 @@ const os = jest.requireActual('os');
 
 const osMock = Object.assign({}, os, {
   homedir: function () {
-    return 'C:\\Users\\MOCK_HOME_DIR';
+    return 'C:/Users/MOCK_HOME_DIR';
   },
   tmpdir: function () {
-    return 'C:\\Users\\DUMMY\\AppData\\Local\\Temp';
+    return 'C:/Users/DUMMY/AppData/Local/Temp';
   }
 });
 
