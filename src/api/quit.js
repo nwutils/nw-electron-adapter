0c53b3380b35b9beba6d5f85722d0e754b37a816
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-09-06 18:21:19 -0400

    Update quit.js

--- src/api/quit.js
+++ src/api/quit.js
@@ -4,6 +4,14 @@
   https://www.electronjs.org/docs/api/app#appquit
 
   `app.quit()`
+
+  Try to close all windows. The before-quit event will be emitted first.
+  If all windows are successfully closed, the will-quit event will be
+  emitted and by default the application will terminate.
+
+  This method guarantees that all beforeunload and unload event handlers
+  are correctly executed. It is possible that a window cancels the
+  quitting by returning false in the beforeunload event handler.
 */
 
 /**
