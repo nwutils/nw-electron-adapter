90be920ecd56df5728687ad74b3b8130769c982c
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-09-08 14:18:05 -0400

    Update addRecentDocument.js

--- src/api/addRecentDocument.js
+++ src/api/addRecentDocument.js
@@ -1,15 +1,21 @@
 
 /*
   Electron Docs v10.1.1
-  https://www.electronjs.org/docs/api/app#appaddrecentdocument
+  https://www.electronjs.org/docs/api/app#appaddrecentdocumentpath-macos-windows
 
-  `app.addRecentDocument()`
+  `app.addRecentDocument(path)` Windows, OSX
+    * `path` String
+
+  Adds `path` to the recent documents list.
+
+  This list is managed by the OS. On Windows, you can visit the list from the task
+  bar, and on macOS, you can visit it from dock menu.
 */
 
 /**
- * [Description]
+ * Adds the given path to the OS's list of recently opened documents.
  *
- * @return {undefined}  [Description]
+ * @return {undefined}  Nothing returned. Just sets value or throws if error occurred.
  */
 const addRecentDocument = function () {
 };
