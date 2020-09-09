9a057d591685cc5704e10212657237e1be718396
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-09-09 09:45:08 -0400

    Update getFileIcon.js

--- src/api/getFileIcon.js
+++ src/api/getFileIcon.js
@@ -1,9 +1,29 @@
 
 /*
   Electron Docs v10.1.1
-  https://www.electronjs.org/docs/api/app#appgetfileicon
+  https://www.electronjs.org/docs/api/app#appgetfileiconpath-options
 
-  `app.getFileIcon()`
+  `app.getFileIcon(path[, options])`
+
+  * `path` String
+  * `options` Object (optional)
+    * `size` String
+      * `small` - 16x16
+      * `normal` - 32x32
+      * `large` - 48x48 on Linux, 32x32 on Windows, unsupported on OSX.
+
+  Returns `Promise<NativeImage>` - fulfilled with the app's icon, which is a NativeImage.
+
+  * https://github.com/electron/electron/blob/master/docs/api/native-image.md
+
+  Fetches a path's associated icon.
+
+  On Windows, there a 2 kinds of icons:
+
+  * Icons associated with certain file extensions, like `.mp3`, `.png`, etc.
+  * Icons inside the file itself, like `.exe`, `.dll`, `.ico`.
+
+  On Linux and OSX, icons depend on the application associated with file mime type.
 */
 
 /**
